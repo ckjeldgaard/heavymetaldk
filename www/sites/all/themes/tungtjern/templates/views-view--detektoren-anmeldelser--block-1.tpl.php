e87781a175d2aac5099b6c37a665d2f7c994125f
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2018-03-22 11:10:56 +0100

    Styling number of Detektoren reviews.

--- www/sites/all/themes/tungtjern/templates/views-view--detektoren-anmeldelser--block-1.tpl.php
+++ www/sites/all/themes/tungtjern/templates/views-view--detektoren-anmeldelser--block-1.tpl.php
@@ -1,4 +1,4 @@
-<h2 id="anmeldelser"><?php print t('Detektoren reviews') . ' (' . $view->total_rows . ')'; ?></h2>
+<h2 id="anmeldelser"><?php print t('Detektoren reviews'); ?> <span><?php print '(' . $view->total_rows . ')'; ?></span></h2>
   <?php if ($rows): ?>
     <div class="view-content">
       <?php print $rows; ?>
