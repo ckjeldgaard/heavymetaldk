cbe64695740d5c0fe0b9d783692f76467025cf29
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2018-03-28 11:48:16 +0200

    Fetching nodes to display in frontpage

--- www/sites/all/modules/custom/frontpage/block--frontpage.tpl.php
+++ www/sites/all/modules/custom/frontpage/block--frontpage.tpl.php
@@ -4,33 +4,20 @@
     ?></pre> -->
 
   <section class="promoted slider">
-    <?php for ($i = 0; $i < 4; $i++): ?>
+    <?php foreach($content['promoted'] as $n): ?>
       <div>
-        <?php $node = node_load(32321); ?>
-        <?php $node_view = node_view($node, 'topfront'); ?>
-        <?php print render($node_view); ?>
+        <?php print $n['view']; ?>
       </div>
-    <?php endfor; ?>
+    <?php endforeach; ?>
   </section>
 
   <h2><?php print t('Danish content'); ?></h2>
   <section class="danish pure-g focus slider">
-    <?php for ($i = 0; $i < 6; $i++): ?>
-      <div class="pure-u-1-1 pure-u-sm-1-3 l-box <?php if ($i % 3 == 0): ?>first<?php endif; ?> <?php if ($i % 3 == 2): ?>last<?php endif; ?>">
-        <?php
-        $n = 32521;
-        if ($i % 2 == 1) {
-          $n = 32596;
-        }
-        if ($i % 3 == 2) {
-          $n = 32450;
-        }
-        ?>
-        <?php $node = node_load($n); ?>
-        <?php $node_view = node_view($node, 'focus'); ?>
-        <?php print render($node_view); ?>
+    <?php foreach($content['danish'] as $n): ?>
+      <div class="pure-u-1-1 pure-u-sm-1-3 l-box <?php print $n['classes']; ?>">
+        <?php print $n['view']; ?>
       </div>
-    <?php endfor; ?>
+    <?php endforeach; ?>
   </section>
 
   <h2><?php print t('International content'); ?></h2>
